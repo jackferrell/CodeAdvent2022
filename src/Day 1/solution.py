b2f04b16545f7ee76850af5ca97302ce034ef141
jackferrellncsu <jacksax290@gmail.com>
2022-12-24 11:53:13 -0600

    day 1 solution

--- src/Day 1/solution.py
+++ src/Day 1/solution.py
@@ -1,6 +1,21 @@
 input_path = 'input1.txt'
 
-with open(input_path) as input_file:
-    contents = input_file.readlines()
+elf_cals = 0
+elf_list = []
 
-    print(contents)
+with open(input_path) as input_file:
+    for line in input_file:
+
+        if line != '\n':
+            elf_cals += int(line.strip('\n'))
+        else:
+            elf_list.append(elf_cals)
+            elf_cals = 0
+
+    #Catches eof line:
+    elf_list.append(elf_cals)
+            
+elf_list.sort()
+print('Max:', elf_list[-1])
+
+print('Sum:', sum(elf_list[-3:]))
